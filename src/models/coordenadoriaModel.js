a3559b4a178f43cdd6e12927bc7c2647d890ce85
Juan Cardoso da Silva <juancardosodasilva@gmail.com>
2021-08-31 20:24:48 -0300

    Arrumado umas coisas do Back-end

--- src/models/coordenadoriaModel.js
+++ src/models/coordenadoriaModel.js
@@ -2,29 +2,29 @@ const connection = require('../config/connection');
 const queryExecuter = require('../helpers/queryExecuter');
 
 module.exports = {
-    insert({nome_usuario, email, senha, nome, numero_usp, nivel}) { 
-        console.log(nome_usuario)
-        return queryExecuter(connection, "INSERT INTO coordenadoria (nome_usuario, email, senha, nome, numero_usp, nivel) VALUES (?, ?, ?, ?, ?, ?)", [nome_usuario, email, senha, nome, numero_usp, nivel])
-    },
+  insert({ nome_usuario, email, senha, nome, numero_usp, nivel }) {
+    console.log(nome_usuario)
+    return queryExecuter(connection, "INSERT INTO coordenadoria (nome_usuario, email, senha, nome, numero_usp, nivel) VALUES (?, ?, ?, ?, ?, ?)", [nome_usuario, email, senha, nome, numero_usp, nivel])
+  },
 
-    getAll() {
-        return queryExecuter(connection, "SELECT * FROM coordenadoria", [])
-    },  
+  getAll() {
+    return queryExecuter(connection, "SELECT * FROM coordenadoria", [])
+  },
 
-    getById(id) {
-        return queryExecuter(connection, "SELECT * FROM coordenadoria WHERE id = ?", [id])
-    },
+  getById(id) {
+    return queryExecuter(connection, "SELECT * FROM coordenadoria WHERE id = ?", [id])
+  },
 
-    getByEmail(email) {
-        console.log(email);
-        return queryExecuter(connection, "SELECT * FROM coordenadoria WHERE email = ?", [email])
-    },
+  getByEmail(email) {
+    console.log(email);
+    return queryExecuter(connection, "SELECT * FROM coordenadoria WHERE email = ?", [email])
+  },
 
-    delete(id) {
-        try {
-            return queryExecuter(connection, "DELETE FROM coordenadoria WHERE id = ?", [id]);
-        } catch(e) {
-            throw e;
-        }
+  delete(id) {
+    try {
+      return queryExecuter(connection, "DELETE FROM coordenadoria WHERE id = ?", [id]);
+    } catch (e) {
+      throw e;
     }
+  }
 }
