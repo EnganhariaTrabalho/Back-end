eef2d88735b65468f41ed02e1f5fe0a28665babb
Juan Cardoso da Silva <juancardosodasilva@gmail.com>
2021-09-20 18:04:54 -0300

    voltado ao padrão pq o banco de dados me odeia

--- src/models/formularioModel.js
+++ src/models/formularioModel.js
@@ -4,27 +4,27 @@ const queryExecuter = require('../helpers/queryExecuter');
 
 module.exports = {
     insert({cod_formulario, numero_usp_aluno, numero_usp_professor, 
-        semestre_curso, ultima_atualizacao_do_lattes, resultado_ultima_avaliacao, aprovacao_obrigatorias , 
+        semestre_curso, ultima_atualizacao_do_lattes, resultado_ultima_avaliacao, aprovacao_obrigatorias, 
         aprovacao_optativas, conceitos, disciplinas_repovadas_curso, disciplinas_repovadas_semestre, 
-        exame_proeficiencia, exame_de_qualificacao, limite_max_qualificacao, tempo_limite_deposito, 
+        exame_proeficiencia, exame_de_qualificacao, limite_max_qualificacao, tempo_limete_deposito, 
         artigos_publicados, artigos_espera, artigo_preparacao, estagio_atual_pesquisa, congresso, 
-        congresso_exterior, pesquisa_exterior, declaracao}) {
+        congresso_extorior, pesquisa_exterior, declaracao}) {
 
         let query  = "INSERT INTO formulario";
         query +=" (cod_formulario, numero_usp_aluno, numero_usp_professor, semestre_curso," + 
-                   "ultima_atualizacao_do_lattes, resultado_ultima_avaliacao, aprovacao_obrigatorias , aprovacao_optativas," + 
+                   "ultima_atualizacao_do_lattes, resultado_ultima_avaliacao, aprovacao_obrigatorias, aprovacao_optativas," + 
                    "conceitos, disciplinas_repovadas_curso, disciplinas_repovadas_semestre, exame_proeficiencia," + 
-                   "exame_de_qualificacao,limite_max_qualificacao, tempo_limite_deposito, artigos_publicados, artigos_espera," + 
-                   "artigo_preparacao, estagio_atual_pesquisa,congresso ,congresso_exterior, pesquisa_exterior, declaracao)" + 
+                   "exame_de_qualificacao,limite_max_qualificacao, tempo_limete_deposito, artigos_publicados, artigos_espera," + 
+                   "artigo_preparacao, estagio_atual_pesquisa,congresso ,congresso_extorior, pesquisa_exterior, declaracao)" + 
                    "VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
 
         return queryExecuter(connection, query, [
             cod_formulario, numero_usp_aluno, numero_usp_professor, 
-        semestre_curso, ultima_atualizacao_do_lattes, resultado_ultima_avaliacao, aprovacao_obrigatorias , 
+        semestre_curso, ultima_atualizacao_do_lattes, resultado_ultima_avaliacao, aprovacao_obrigatorias, 
         aprovacao_optativas, conceitos, disciplinas_repovadas_curso, disciplinas_repovadas_semestre, 
-        exame_proeficiencia, exame_de_qualificacao, limite_max_qualificacao, tempo_limite_deposito, 
+        exame_proeficiencia, exame_de_qualificacao, limite_max_qualificacao, tempo_limete_deposito, 
         artigos_publicados, artigos_espera, artigo_preparacao, estagio_atual_pesquisa, congresso, 
-        congresso_exterior, pesquisa_exterior, declaracao 
+        congresso_extorior, pesquisa_exterior, declaracao 
         ]);
     },
 
@@ -68,13 +68,13 @@ module.exports = {
             query +=" semestre_curso = ?," + 
                    "ultima_atualizacao_do_lattes = ?, resultado_ultima_avaliacao = ?, aprovacao_obrigatorias= ?, aprovacao_optativas= ?, " + 
                    "conceitos= ?, disciplinas_repovadas_curso= ?, disciplinas_repovadas_semestre= ?, exame_proeficiencia= ?, " + 
-                   "exame_de_qualificacao= ?,limite_max_qualificacao= ?, tempo_limite_deposito= ?, artigos_publicados= ?, artigos_espera= ?, " + 
+                   "exame_de_qualificacao= ?,limite_max_qualificacao= ?, tempo_limete_deposito= ?, artigos_publicados= ?, artigos_espera= ?, " + 
                    "artigo_preparacao= ?, estagio_atual_pesquisa= ?,congresso = ?,congresso_extorior= ?, pesquisa_exterior= ?, declaracao= ? " + 
                    "WHERE cod_formulario = ?";
             return queryExecuter(connection, query, [
             formulario.semestre_curso, formulario.ultima_atualizacao_do_lattes, formulario.resultado_ultima_avaliacao, formulario.aprovacao_obrigatorias, 
             formulario.aprovacao_optativas, formulario.conceitos, formulario.disciplinas_repovadas_curso, formulario.disciplinas_repovadas_semestre, 
-            formulario.exame_proeficiencia, formulario.exame_de_qualificacao, formulario.limite_max_qualificacao, formulario.tempo_limite_deposito, 
+            formulario.exame_proeficiencia, formulario.exame_de_qualificacao, formulario.limite_max_qualificacao, formulario.tempo_limete_deposito, 
             formulario.artigos_publicados, formulario.artigos_espera, formulario.artigo_preparacao, formulario.estagio_atual_pesquisa, formulario.congresso, 
             formulario.congresso_extorior, formulario.pesquisa_exterior, formulario.declaracao, cod_formulario
             ]);
